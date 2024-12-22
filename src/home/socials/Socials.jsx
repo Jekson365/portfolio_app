c6a53d841865b683e66b428df9fc80b669d2193f
Jeko <jemali.ergemlidze.1@btu.edu.ge>
2024-12-22 16:28:33 +0400

    cc

--- src/home/socials/Socials.jsx
+++ src/home/socials/Socials.jsx
@@ -14,7 +14,7 @@ export const Socials = () => {
         {
             icon: <ArticleIcon/>,
             name: 'Resume',
-            link: '../../../public/jemali_ergemlidze.pdf',
+            link: '../../../jemali_ergemlidze.pdf',
             downloadable: true,
 
         },
