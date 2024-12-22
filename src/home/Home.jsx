75a1a29d786461cd96f306f89406d839d62cfa29
Jeko <jemali.ergemlidze.1@btu.edu.ge>
2024-12-22 10:13:51 +0400

    cc

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
         <h1>რატის Patara პენისი აქ! ddsaasdasdasd</h1>
-        <h1>ganaxlebuli asdgverdiSSs</h1>
+        <h1>ganaxlebuli asdgverdiSdasdSs</h1>
     </>
   )
 }
