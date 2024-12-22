6709ade8659bb6491251698a2322b41990e51836
Jeko <jemali.ergemlidze.1@btu.edu.ge>
2024-12-22 09:56:51 +0400

    cc

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <>
-        <h1>რატის Patara პენისი აქ! dasdasdasd</h1>
+        <h1>რატის Patara პენისი აქ! ddsaasdasdasd</h1>
         <h1>ganaxlebuli asdgverdiSSs</h1>
     </>
   )
