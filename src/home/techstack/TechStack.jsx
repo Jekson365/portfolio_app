d05cd8092499609ea1578a25310a4544299e991b
Jeko <jeko.erg@gamil.com>
2025-02-14 19:12:49 +0400

    new project

--- src/home/techstack/TechStack.jsx
+++ src/home/techstack/TechStack.jsx
@@ -1,36 +1,50 @@
-import {Box, Grid} from '@mui/material'
-import {useEffect} from "react";
+import { Box, Grid } from "@mui/material";
+import { useEffect } from "react";
 
 export const TechStack = () => {
-    const stacks = [
-        'PHP', 'Laravel', 'React', 'Python', 'Javascript', 'HTML', 'CSS', 'Ruby', 'Ruby on rails', 'CI/CD',
-        'Git', 'SQL', 'Postgresql', 'Mysql'
-    ]
-    return (
-        <>
-            <Box mt={3}></Box>
-            <div className={'headline'}
-                 data-aos={'fade'}
-                 data-aos-duration={1600}
-            >Tech Stack</div>
-            <div className={'underline c-secondary'}
-                 data-aos={'fade'}
-                 data-aos-duration={1600}
-            >Technologies that I mastered</div>
-            <Box mt={6}></Box>
-            <Grid container spacing={2}>
-                {stacks.map((e) => {
-                    return (
-                        <>
-                            <Grid item xs={2}>
-                                <div className={'tech-item'}>
-                                    {e}
-                                </div>
-                            </Grid>
-                        </>
-                    )
-                })}
-            </Grid>
-        </>
-    )
-}
+  const stacks = [
+    "C#",
+    ".NET",
+    "Postgresql",
+    "Git",
+    "SQL",
+    "PHP",
+    "Laravel",
+    "React",
+    "Python",
+    "Javascript",
+    "HTML",
+    "CSS",
+    "Ruby",
+    "Ruby on rails",
+    "CI/CD",
+    "Mysql",
+  ];
+  return (
+    <>
+      <Box mt={3}></Box>
+      <div className={"headline"} data-aos={"fade"} data-aos-duration={1600}>
+        Tech Stack
+      </div>
+      <div
+        className={"underline c-secondary"}
+        data-aos={"fade"}
+        data-aos-duration={1600}
+      >
+        Technologies that I mastered
+      </div>
+      <Box mt={6}></Box>
+      <Grid container spacing={2}>
+        {stacks.map((e) => {
+          return (
+            <>
+              <Grid item xs={2}>
+                <div className={"tech-item"}>{e}</div>
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
--- src/projects/PersonalProjects.jsx
+++ src/projects/PersonalProjects.jsx
@@ -1,114 +1,115 @@
-import {Grid, Box, Stack} from "@mui/material";
-import {FaPython} from "react-icons/fa";
-import {DiRuby} from "react-icons/di";
-import {FaReact} from "react-icons/fa";
-import quizzGif from '../assets/gifs/quizz.gif';
-import dozaiGif from '../assets/gifs/dozai.gif';
-import cafiendGif from '../assets/gifs/cafiend.gif';
-
+import { Grid, Box, Stack } from "@mui/material";
+import { FaPython } from "react-icons/fa";
+import { DiRuby } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import quizzGif from "../assets/gifs/quizz.gif";
+import dozaiGif from "../assets/gifs/dozai.gif";
+import cafiendGif from "../assets/gifs/cafiend.gif";
+import empGif from "../assets/gifs/emp.gif";
+import { TbBrandCSharp } from "react-icons/tb";
+import { AiOutlineDotNet } from "react-icons/ai";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 export const PersonalProjects = () => {
-    const arr = [
-        {
-            img: quizzGif,
-            title: "Georgian Quizz",
-            description: 'questions tests how much georgian is in you',
-            tools: [
-                <DiRuby/>,
-                <FaReact/>
-            ],
-            github: 'https://github.com/Jekson365/guess_api',
-            live:'http://guess.testpuzzle.ge/'
-        },
-        {
-            img: dozaiGif,
-            title: "Dozai",
-            description: 'Design is Take from webflow.com.',
-            tools: [
-                <FaReact/>
-            ],
-            github: 'https://github.com/Jekson365/dozai',
-            live:'http://dozai.jergemlidze.com.ge'
-        },
-        {
-            img: cafiendGif,
-            title: "Cafiend",
-            description: 'Design is Take from webflow.com.',
-            tools: [
-                <FaReact/>
-            ],
-            github: 'https://github.com/Jekson365/cafiend',
-            live:'http://cafiend.jergemlidze.com.ge/'
-        },
-    ]
-    return (
-        <>
-            <Box mt={4}></Box>
-            <div className={'headline'}>Projects</div>
-            <div className={'underline c-gray'}>My personal Projects</div>
-            <Grid container spacing={5} mt={2}>
-                {arr.map((e) => {
-                    return (
-                        <Grid xs={6} item
-
-                        >
-                            <Stack
-                                data-aos={'fade-up'}
-                                direction={'column'}
-                                alignItems={'flex-start'}
-                                minHeight={'450px'}
-                                maxHeight={'600px'}
-                                className={'project-item'}>
-                                <div className={'project-item-image'}
-                                     style={{backgroundImage: `url('${e.img}')`, height: "100%"}}
-                                >
-                                </div>
-                                <Stack
-                                    direction={'column'}
-                                    justifyContent={'space-between'}
-                                    style={{
-                                        height:"200px",
-                                    }}
-                                >
-                                    <Stack gap={'5px'} mt={1}>
-                                        <h4 className={'c-orange'}>{e.title}</h4>
-                                        <div className={'c-gray'}>{e.description}</div>
-                                    </Stack>
-                                    <Stack
-                                        direction={'row'}
-                                        gap={'5px'}
-                                        mt={2}
-                                    >
-                                        {e.tools.map((t) => {
-                                            return (
-                                                <>
-                                                    <div
-                                                        className={"own-project-icon"}
-                                                    >
-                                                        {t}
-                                                    </div>
-                                                </>
-                                            )
-                                        })}
-                                    </Stack>
-                                    <Stack
-                                        mt={1}
-                                        direction={'row'}
-                                        gap={'5px'}
-                                    >
-                                        <button className={'button-min-main'}>
-                                            <a href={e.live}>Live</a>
-                                        </button>
-                                        <button className={'button-min-main'}>
-                                            <a href={e.github}>Source</a>
-                                        </button>
-                                    </Stack>
-                                </Stack>
-                            </Stack>
-                        </Grid>
-                    )
-                })}
+  const arr = [
+    {
+      img: empGif,
+      title: "Employee Managment",
+      description: "application which helps manage employees and their tasks",
+      tools: [
+        <FaReact />,
+        <TbBrandCSharp />,
+        <AiOutlineDotNet />,
+        <BiLogoPostgresql />,
+      ],
+      github: "https://github.com/Jekson365/EmpManagment",
+      live: "https://foo.jergemlidze.com.ge",
+    },
+    {
+      img: quizzGif,
+      title: "Georgian Quizz",
+      description: "questions tests how much georgian is in you",
+      tools: [<DiRuby />, <FaReact />, <BiLogoPostgresql />],
+      github: "https://github.com/Jekson365/guess_api",
+      live: "http://guess.testpuzzle.ge/",
+    },
+    {
+      img: dozaiGif,
+      title: "Dozai",
+      description: "Design is Taken from webflow.com.",
+      tools: [<FaReact />],
+      github: "https://github.com/Jekson365/dozai",
+      live: "http://dozai.jergemlidze.com.ge",
+    },
+    {
+      img: cafiendGif,
+      title: "Cafiend",
+      description: "Design is Taken from webflow.com.",
+      tools: [<FaReact />],
+      github: "https://github.com/Jekson365/cafiend",
+      live: "http://cafiend.jergemlidze.com.ge/",
+    },
+  ];
+  return (
+    <>
+      <Box mt={4}></Box>
+      <div className={"headline"}>Projects</div>
+      <div className={"underline c-gray"}>My personal Projects</div>
+      <Grid container spacing={5} mt={2}>
+        {arr.map((e) => {
+          return (
+            <Grid xs={6} item>
+              <Stack
+                data-aos={"fade-up"}
+                direction={"column"}
+                alignItems={"flex-start"}
+                minHeight={"450px"}
+                maxHeight={"600px"}
+                className={"project-item"}
+              >
+                <div
+                  className={"project-item-image"}
+                  style={{ backgroundImage: `url('${e.img}')`, height: "100%" }}
+                ></div>
+                <Stack
+                  direction={"column"}
+                  justifyContent={"space-between"}
+                  style={{
+                    height: "200px",
+                  }}
+                >
+                  <Stack gap={"5px"} mt={1}>
+                    <h4 className={"c-orange"}
+                    style={{fontSize:"25px"}}
+                    >{e.title}</h4>
+                    <div className={"c-gray"}
+                    style={{fontSize:"20px"}}
+                    >{e.description}</div>
+                  </Stack>
+                  <Stack direction={"row"} gap={"5px"} mt={2}>
+                    {e.tools.map((t) => {
+                      return (
+                        <>
+                          <div className={"own-project-icon"}
+                          >{t}</div>
+                        </>
+                      );
+                    })}
+                  </Stack>
+                  <Stack mt={1} direction={"row"} gap={"5px"}>
+                    <button className={"button-min-main"}>
+                      <a href={e.live}>Live</a>
+                    </button>
+                    <button className={"button-min-main"}>
+                      <a href={e.github}>Source</a>
+                    </button>
+                  </Stack>
+                </Stack>
+              </Stack>
             </Grid>
-        </>
-    )
-}
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
